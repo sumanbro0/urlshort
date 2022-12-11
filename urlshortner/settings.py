59f6a1a4ab4a731f0505670fd61a299cd8331878
sumanbro0m <pandey.suman258@gmail.com>
2022-12-11 21:52:26 +0545

    sec

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -121,7 +121,5 @@ STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "urlshortner/static")]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
