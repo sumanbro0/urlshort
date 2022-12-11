ca9b1e306be159fe7fba06ff7d8fd6ca3c1fb930
sumanbro0m <pandey.suman258@gmail.com>
2022-12-11 22:40:00 +0545

    eighth

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "urlshortner.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "Wz6DrI1XGIiB7bg6486w",
+        "HOST": "containers-us-west-155.railway.app",
+        "PORT": "5983",
     }
 }
 
